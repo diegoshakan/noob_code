e64f70d34a4ad53aee9485a10d51dcb98e77717c
Diego Araujo <diego_shakan@hotmail.com>
2021-03-20 20:47:25 -0300

    ternario ruby

---
+++ curso-noob-ruby/melhorias/ternario.rb
@@ -0,0 +1,17 @@
+nome = "Diego"
+n = 4
+
+if nome.eql?("Diego")
+  puts "Dono do canal Noob Code"
+else
+  puts "Visitante"
+end
+
+condição ? verdadeiro : falso
+
+puts nome.eql?("Diego") ? "Dono do canal Noob Code" : "Visitante"
+resultado = nome.eql?("Diego") ? "Dono do canal Noob Code - resultado" : "Visitante - resultado"
+puts resultado
+
+soma = n.eql?(5) ? n + 10 : n - 1
+puts soma
